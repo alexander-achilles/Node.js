f543d65754f2f72b9743a185111ea655b1dac95a
alexander-achilles <mrinmoym247@gmail.com>
2021-01-03 22:06:18 +0530

    Stored images in the database and served it statically

--- File/app.js
+++ File/app.js
@@ -38,6 +38,14 @@ const fileStorage = multer.diskStorage({
   },
 });
 
+const fileFilter=(req,file,cb)=>{
+  if(file.mimetype==='image/png' || file.mimetype==='image/jpg' || file.mimetype==='image/jpeg'){
+    cb(null,true);
+  }else{
+    cb(null,false);
+  }
+}
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -47,9 +55,11 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(multer({storage: fileStorage}).single("image"));
+app.use(multer({storage: fileStorage, fileFilter:fileFilter}).single("image"));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use(
   session({
     secret: 'my secret',
